d3bc3a3cf480abbc96e42a92e6ac1b944b6826f3
murphy.yu <yuwj86@gmail.com>
2013-06-17 15:30:04 +0800

    no message

--- src/Vct/VpnBundle/Controller/KanriController.php
+++ src/Vct/VpnBundle/Controller/KanriController.php
@@ -31,7 +31,7 @@ class KanriController extends Controller {
 	}
 
     public function aboutAction(Request $request) {
-        return $this->render('vctVpnBundle:Kanri:about.html.twig');
+        return $this->render('VctVpnBundle:Kanri:about.html.twig');
     }
 
 	public function newAction(Request $request) {
@@ -136,32 +136,32 @@ class KanriController extends Controller {
 		
 	}
 	
-	public function rtnJsonAction(Request $request) {
-		$param_get = $request->query->all();
+//	public function rtnJsonAction(Request $request) {
+//		$param_get = $request->query->all();
+//
+//		$response = new Response(json_encode(array($param_get)));
+//		$response->headers->set('Content-Type', 'application/json');
+//		return $response;
+//	}
 	
-		$response = new Response(json_encode(array($param_get)));
-		$response->headers->set('Content-Type', 'application/json');
-		return $response;
-	}
-	
-	public function showUserAction(Request $request) {
-	
-		$param_get = $request->query->all();
-		$user_id = $param_get['id'];
-	
-		// 		var_dump($user_id);
-	
-		$em = $this->getDoctrine()->getEntityManager();
-		$repo = $em->getRepository('VctVpnBundle:TUser');
-		$user = $repo->findOneBy(array('id' => $user_id));
-	
-	
-		$arr['user'] = $user;
-		var_dump($user);
-		// 		$arr['user_ids'] = 'ab';
-	
-		return $this->render('VctVpnBundle:Vctvpn:info.html.twig',$arr);
-	}
+//	public function showUserAction(Request $request) {
+//
+//		$param_get = $request->query->all();
+//		$user_id = $param_get['id'];
+//
+//		// 		var_dump($user_id);
+//
+//		$em = $this->getDoctrine()->getEntityManager();
+//		$repo = $em->getRepository('VctVpnBundle:TUser');
+//		$user = $repo->findOneBy(array('id' => $user_id));
+//
+//
+//		$arr['user'] = $user;
+//		var_dump($user);
+//		// 		$arr['user_ids'] = 'ab';
+//
+//		return $this->render('VctVpnBundle:Vctvpn:info.html.twig',$arr);
+//	}
 	
 	
 
--- src/Vct/VpnBundle/Repository/TUserRepository.php
+++ src/Vct/VpnBundle/Repository/TUserRepository.php
@@ -3,43 +3,28 @@
 namespace Vct\VpnBundle\Repository;
 use Doctrine\ORM\EntityRepository;
 
-class TUserRepository extends EntityRepository {
+class TUserRepository extends EntityRepository
+{
+    public function getTUserWithCount($count)
+    {
+        return $this->getEntityManager()
+            ->createQuery('SELECT t FROM VctVpnBundle:TUser t')
+            ->setMaxResults($count)->getResult();
+    }
 
-	public function getTUserWithCount($count) {
-		return $this->getEntityManager()
-				// 				->createQuery('SELECT * FROM VctVpnBundle:TUser limit :count')
-				// 				->setParameters(array('count' => $count))->getResult();
-				->createQuery('SELECT t FROM VctVpnBundle:TUser t')
-				->setMaxResults($count)->getResult();
+    public function getTUserInArray($zArray)
+    {
+        return $this->getEntityManager()
+            ->createQuery(
+                'SELECT t FROM VctVpnBundle:TUser t where t.id IN (:zArray)')
+            ->setParameter('zArray', $zArray)->getResult();
+    }
 
-		// 		$em = $this->getDoctrine()->getManager();
-		// 		$query = $em->createQuery(
-		// 				'SELECT p FROM AcmeStoreBundle:Product p WHERE p.price > :price ORDER BY p.price ASC'
-		// 		)->setParameter('price', '19.99');
-
-	}
-
-	public function getTUserInArray($zArray) {
-		return $this->getEntityManager()
-				// 				->createQuery('SELECT * FROM VctVpnBundle:TUser limit :count')
-				// 				->setParameters(array('count' => $count))->getResult();
-				->createQuery(
-						'SELECT t FROM VctVpnBundle:TUser t where t.id IN (:zArray)')
-				->setParameter('zArray', $zArray)->getResult();
-
-		// 		$em = $this->getDoctrine()->getManager();
-		// 		$query = $em->createQuery(
-		// 				'SELECT p FROM AcmeStoreBundle:Product p WHERE p.price > :price ORDER BY p.price ASC'
-		// 		)->setParameter('price', '19.99');
-
-	}
-
-	public function deleteTUserInArray($zArray) {
-
-		return $this->getEntityManager()
-				->createQuery(
-						'DELETE VctVpnBundle:TUser t where t.id IN (:zArray)')
-				->setParameter('zArray', $zArray)->getResult();
-	}
-
+    public function deleteTUserInArray($zArray)
+    {
+        return $this->getEntityManager()
+            ->createQuery(
+                'DELETE VctVpnBundle:TUser t where t.id IN (:zArray)')
+            ->setParameter('zArray', $zArray)->getResult();
+    }
 }
